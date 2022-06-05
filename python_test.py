1174008667950c9967ef098c9db207150a1767ec
ISAIAS BARBOSA <64814667+ISAIAS-debug@users.noreply.github.com>
2022-06-05 13:50:27 -0300

    Update2
    código de verificação de desempenho

--- python_test.py
+++ python_test.py
@@ -1,3 +1,21 @@
-from datetime import datetime
+from multiprocessing import Pool
+import time
 
-print(f'Hoje é {datetime} e esse é o meu primeiro script python commitado')
+CONTADOR = 500000
+
+
+def contando(n):
+    while n > 0:
+        n -= 1
+
+
+if __name__ == '__main__':
+    pool = Pool(processes=2)
+    inicio = time.time()
+    p1 = pool.apply_async(contando, [CONTADOR//2])
+    p2 = pool.apply_async(contando, [CONTADOR//2])
+    pool.close()
+    pool.join()
+    fim = time.time()
+
+    print(f'O tempo gasto para dois processos em Poll foi de : {fim - inicio}')
